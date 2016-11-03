85fd06fdd7c53127ca16512cca97363cb305c7dd
xuekai <3440395@qq.com>
2016-11-03 11:15:30 +0800

    增加了constant类

---
+++ app/src/main/java/com/xk/xiaomiweather/Constant.java
@@ -0,0 +1,11 @@
+package com.xk.xiaomiweather;
+
+/**
+ * Created by xuekai on 2016/11/3.
+ */
+
+public class Constant {
+    public static final String BASE_ENVICLOUD_URL="http://service.envicloud.cn:8082";
+    public static final String envicloudID="EGSZNDQWMZK1MTQ3NZKXNJIYNDAYOQ==";
+    public static final String juheID="";
+}
